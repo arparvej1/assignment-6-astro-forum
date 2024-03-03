15bc4cf1b07c38e348dc0de03cfb428d40c28f24
arparvej1 <parvej8pro@gmail.com>
2024-03-03 17:15:46 +0600

    Let’s Discuss author responsive

--- js/astro-forum.js
+++ js/astro-forum.js
@@ -15,13 +15,6 @@ const displayPosts = posts => {
   const postsContainer = document.getElementById('postsContainer');
   postsContainer.innerHTML = '';
 
-  // if (phones.length > 9 && showAllPhones == false) {
-  //   phones = phones.slice(0, 9);
-  //   showAllBtn.classList.remove('hidden');
-  // } else {
-  //   showAllBtn.classList.add('hidden');
-  // }
-
   posts.forEach(post => {
     let active = '';
     post.isActive ? active = 'bg-[#10B981]' : active = 'bg-[#FF3434]';
@@ -30,26 +23,31 @@ const displayPosts = posts => {
     postCard.classList = `flex flex-col lg:flex-row bg-[#F3F3F5] p-8 rounded-2xl gap-5 border-2 border-[#F3F3F5] hover:border-[#10B981] hover:bg-[#10b98115]`;
     postCard.innerHTML = `
     <!-- Card Left -->
-    <div>
+    <div class="flex gap-4 items-center lg:items-start">
       <div class="relative">
         <div class="w-24 rounded-2xl overflow-hidden">
           <img src="${post.image}" alt="">
         </div>
         <div id="post-author-active"
-          class="w-4 h-4 lg:w-6 lg:h-6 absolute -top-1 lg:-top-2 lg:-right-2 rounded-full ${active}"></div>
+          class="w-4 h-4 lg:w-6 lg:h-6 absolute -top-1 lg:-top-2 lg:-right-2 rounded-full ${active}">
+        </div>
       </div>
+      <p class="flex flex-col lg:flex-row lg:hidden gap-3 lg:gap-10 font-medium">
+        <span>#${post.category}</span>
+        <span>Author : ${post.author.name}</span>
+      </p>
     </div>
     <!-- Card Right -->
     <div class="flex flex-col gap-3">
-      <p class="flex flex-col lg:flex-row gap-3 lg:gap-10 font-medium">
+      <p class="flex flex-col lg:flex-row hidden lg:block gap-3 lg:gap-10 font-medium">
         <span>#${post.category}</span>
         <span>Author : ${post.author.name}</span>
       </p>
       <h3 class="font-bold text-2xl lg:text-3xl">${post.title}</h3>
       <p>${post.description}</p>
       <hr class="border-dashed border-2">
-      <div class="flex flex-col lg:flex-row gap-3 justify-between">
-        <div class="flex flex-col lg:flex-row gap-3 lg:gap-5">
+      <div class="flex gap-3 justify-between">
+        <div class="flex gap-3 lg:gap-5">
           <div class="flex gap-2">
             <img src="/images/icons/comment.svg" alt="">
             <span>${post.comment_count}</span>
@@ -91,20 +89,8 @@ const displayLatestPosts = posts => {
   const postsContainer = document.getElementById('latest-posts-card');
   postsContainer.innerHTML = '';
 
-  // if (phones.length > 9 && showAllPhones == false) {
-  //   phones = phones.slice(0, 9);
-  //   showAllBtn.classList.remove('hidden');
-  // } else {
-  //   showAllBtn.classList.add('hidden');
-  // }
-
-  console.log(posts[0]);
-
   for (let i = 0; i < posts.length; i++) {
     const post = posts[i];
-
-    console.log(post.author.posted_date);
-
     const postCard = document.createElement('div');
     postCard.classList = `flex flex-col lg:w-[33%] gap-3 border-2 p-4 rounded-3xl`;
     postCard.innerHTML = `
@@ -130,7 +116,6 @@ const displayLatestPosts = posts => {
     `;
     postsContainer.appendChild(postCard);
   };
-  // showAllPhones = false;
 }
 
 setTimeout(latestPostsLoad, 2000);
