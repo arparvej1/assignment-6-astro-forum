de3089b394b0931f9dad73dcb5f50d6f42f91168
arparvej1 <parvej8pro@gmail.com>
2024-03-03 19:38:14 +0600

    Mark as read functionaliy add

--- js/astro-forum.js
+++ js/astro-forum.js
@@ -1,14 +1,35 @@
 const loadingPosts = document.getElementById('loading-posts');
 
-const postsLoad = async (searchText) => {
-  // searchText == '' ? searchText = 'iphone' : null;
-  const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`);
+// ------------ Mark as read -------------
+const readPostTitle = document.getElementById('read-post-title-container');
+let readPostMarkCount = 0;
+
+const readMarkClick = (postTitle,view_count) => {
+
+  let title = postTitle.split("---").join("'");
+  console.log(postTitle);
+
+  const postRead = document.createElement('div');
+  postRead.classList = `flex flex-col lg:flex-row gap-2 justify-between items-start lg:items-center bg-white p-5 my-2 rounded-2xl`;
+  postRead.innerHTML = `
+      <p class="font-medium">${title}</p>
+      <p class="flex gap-2 pr-2">
+        <img src="/images/icons/view.svg" alt="">
+        <span>${view_count}</span>
+      </p>
+    `;
+  readPostTitle.appendChild(postRead);
+  readPostMarkCount++;
+  document.getElementById('read-post-count').innerText=`(${readPostMarkCount})`;
+}
+
+
+const postsLoad = async (searchText = '') => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchText}`);
   const data = await res.json();
   const posts = data.posts;
-  // console.log(posts);
-  // loadingPosts.classList.remove('hidden');
+  loadingPosts.classList.remove('hidden');
   displayPosts(posts);
-  loadingPosts.classList.add('hidden');
 }
 
 const displayPosts = posts => {
@@ -21,6 +42,7 @@ const displayPosts = posts => {
 
     const postCard = document.createElement('div');
     postCard.classList = `flex flex-col lg:flex-row bg-[#F3F3F5] p-8 rounded-2xl gap-5 border-2 border-[#F3F3F5] hover:border-[#10B981] hover:bg-[#10b98115]`;
+    let postTitle = post.title.split("'").join('---');
     postCard.innerHTML = `
     <!-- Card Left -->
     <div class="flex gap-4 items-center lg:items-start">
@@ -29,17 +51,17 @@ const displayPosts = posts => {
           <img src="${post.image}" alt="">
         </div>
         <div id="post-author-active"
-          class="w-4 h-4 lg:w-6 lg:h-6 absolute -top-1 lg:-top-2 lg:-right-2 rounded-full ${active}">
+          class="w-4 h-4 lg:w-6 lg:h-6 absolute -top-1 -right-1 lg:-top-2 lg:-right-2 rounded-full ${active}">
         </div>
       </div>
-      <p class="flex flex-col lg:flex-row lg:hidden gap-3 lg:gap-10 font-medium">
+      <p class="flex flex-col lg:hidden gap-3 lg:gap-10 font-medium">
         <span>#${post.category}</span>
         <span>Author : ${post.author.name}</span>
       </p>
     </div>
     <!-- Card Right -->
     <div class="flex flex-col gap-3">
-      <p class="flex flex-col lg:flex-row hidden lg:block gap-3 lg:gap-10 font-medium">
+      <p class="flex hidden lg:block gap-3 lg:gap-10 font-medium">
         <span>#${post.category}</span>
         <span>Author : ${post.author.name}</span>
       </p>
@@ -62,15 +84,22 @@ const displayPosts = posts => {
           </div>
         </div>
         <div>
-          <img src="images/icons/read.png" alt="">
+          <div onclick="readMarkClick('${postTitle}','${post.view_count}')" class="mark-read">
+            <img src="images/icons/read.png" alt="">
+          </div>
         </div>
       </div>
     </div>
     `;
     postsContainer.appendChild(postCard);
   });
-  // showAllPhones = false;
+  loadingPosts.classList.add('hidden');
 }
+
+function handleSearch() {
+  const searchText = document.getElementById('search-field').value;
+  postsLoad(searchText);
+};
 
 setTimeout(postsLoad, 2000);
 
@@ -80,9 +109,8 @@ const loadingLatestPosts = document.getElementById('loading-latest-posts');
 const latestPostsLoad = async () => {
   const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/latest-posts`);
   const data = await res.json();
-  console.log(data);
+  loadingLatestPosts.classList.remove('hidden');
   displayLatestPosts(data);
-  loadingLatestPosts.classList.add('hidden');
 }
 
 const displayLatestPosts = posts => {
@@ -116,6 +144,8 @@ const displayLatestPosts = posts => {
     `;
     postsContainer.appendChild(postCard);
   };
+  loadingLatestPosts.classList.add('hidden');
 }
 
 setTimeout(latestPostsLoad, 2000);
+
